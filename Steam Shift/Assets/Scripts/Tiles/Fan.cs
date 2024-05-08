74d220b4b3d0cd8606b917e5f6a20cb954cbc825
Samuel <samuelhultman@gmail.com>
2024-05-07 19:27:14 -0700

    Working Demo
    Build to be submitted for 5/7 deadline. features a proof of concept with some features to a limited capacity

--- Steam Shift/Assets/Scripts/Tiles/Fan.cs
+++ Steam Shift/Assets/Scripts/Tiles/Fan.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class Fan : MonoBehaviour
 {
+    public bool isOn;
+    // fan will be interactable by the steam spirit to turn it on or off, but for now im low on time so it will make it into next build
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.tag == "Steam")
+        {
+            Debug.Log("fan entered");
+            isOn = true;
+        }
+
         if (collision.tag == "Engineer")
         {
             Debug.Log("fan entered");
@@ -16,6 +25,11 @@ public class Fan : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        if (collision.tag == "Steam")
+        {
+            isOn = false;
+        }
+
         if (collision.tag == "Engineer")
         {
             collision.transform.GetComponent<Engineer>().colliders.Remove("Fan");
